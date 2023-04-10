b2926c98a9ff691a7dc41aca83e42dccfaf8ea51
Vagif Khalilov <lebronirish@gmail.com>
2023-04-10 21:25:36 +0300

    fix declarations

--- src/main/kotlin/inno/jago/ast/decl/Declarations.kt
+++ src/main/kotlin/inno/jago/ast/decl/Declarations.kt
@@ -1,5 +1,6 @@
 package inno.jago.ast.decl
 
+import inno.jago.ast.statement.BlockStatementNode
 import inno.jago.ast.expression.ExpressionNode
 import inno.jago.ast.type.TypeNode
 import inno.jago.entity.Entity
