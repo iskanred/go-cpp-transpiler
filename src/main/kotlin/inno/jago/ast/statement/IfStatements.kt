ecbd963ddca3f80601babe975302316cac72194d
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-04-10 21:20:20 +0300

    add else, simple statements

--- src/main/kotlin/inno/jago/ast/statement/IfStatements.kt
+++ src/main/kotlin/inno/jago/ast/statement/IfStatements.kt
@@ -3,10 +3,24 @@ package inno.jago.ast.statement
 import inno.jago.ast.expression.ExpressionNode
 import inno.jago.lexer.Pos
 
-class IfStatements(
+class IfStatementNode(
     pos: Pos,
     var simpleStatement: SimpleStatementNode,
     var expression: ExpressionNode,
     var block: BlockStatementNode,
     var elseBranch: ElseStatementNode
 ) : StatementNode(pos = pos)
+
+sealed class ElseStatementNode (
+    pos: Pos
+) : StatementNode(pos = pos)
+
+class ElseIfStatementNode (
+    pos: Pos,
+    var ifStmt: IfStatementNode
+) : ElseStatementNode(pos = pos)
+
+class SimpleElseStatementNode (
+    pos: Pos,
+    val block: BlockStatementNode
+) : ElseStatementNode(pos = pos)
