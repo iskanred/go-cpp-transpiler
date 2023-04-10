98099c03f4323fd5bde6474a4778eb40276ed5fd
e.zaripov <e.zaripov@tinkoff.ru>
2023-04-10 22:22:37 +0300

    Add third version of Operand

--- src/main/kotlin/inno/jago/ast/expression/unary_expression/primary_expression/operand/Operand.kt
+++ src/main/kotlin/inno/jago/ast/expression/unary_expression/primary_expression/operand/Operand.kt
@@ -1,5 +1,6 @@
 package inno.jago.ast.expression.unary_expression.primary_expression.operand
 
+import inno.jago.ast.expression.ExpressionNode
 import inno.jago.ast.expression.unary_expression.PrimaryExpressionNode
 import inno.jago.ast.expression.unary_expression.primary_expression.operand.literal_operand.LiteralNode
 import inno.jago.lexer.Pos
@@ -12,3 +13,13 @@ class LiteralOperand(
     pos: Pos,
     val literalNode: LiteralNode
 ) : Operand(pos)
+
+class OperandName(
+    pos: Pos,
+    val identifier: OperandIdentifierNode
+): Operand(pos)
+
+class ExpressionOperand(
+    pos: Pos,
+    val expressionNode: ExpressionNode
+): Operand(pos)
