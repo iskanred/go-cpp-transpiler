3ab4fc338b3b7731ac01a7599be475c377c912ed
Vagif Khalilov <lebronirish@gmail.com>
2023-04-10 21:19:15 +0300

    fix statements ans simple statements

--- src/main/kotlin/inno/jago/ast/statement/SimpleStatements.kt
+++ src/main/kotlin/inno/jago/ast/statement/SimpleStatements.kt
@@ -1,8 +1,36 @@
 package inno.jago.ast.statement
 
+import inno.jago.ast.expression.ExpressionNode
 import inno.jago.lexer.Pos
 
 sealed class SimpleStatementNode (
     pos: Pos
 ) : StatementNode(pos = pos)
 
+class IncDecStatementNode (
+    pos: Pos,
+    val expression: ExpressionNode,
+    val type: IncDec
+) : SimpleStatementNode(pos = pos)
+
+enum class IncDec {
+    INC,
+    DEC
+}
+
+class AssignmentNode (
+    pos: Pos,
+    val left: ExpressionNode,
+    val right: ExpressionNode,
+) : SimpleStatementNode(pos = pos)
+
+class ShortVarDeclNode (
+    pos: Pos,
+    val left: ExpressionNode,
+    val right: ExpressionNode,
+) : SimpleStatementNode(pos = pos)
+
+class ExpressionStatementNode (
+    pos: Pos,
+    val expression: ExpressionNode
+) : SimpleStatementNode(pos = pos)
