7afa0ffa4f2c1a4533205b70cb8259c93376ff53
e.zaripov <e.zaripov@tinkoff.ru>
2023-04-10 22:43:14 +0300

    AST v1

--- src/main/kotlin/inno/jago/ast/expression/unary_expression/primary_expression/operand/literal_operand/LiteralTypeNode.kt
+++ src/main/kotlin/inno/jago/ast/expression/unary_expression/primary_expression/operand/literal_operand/LiteralTypeNode.kt
@@ -1,11 +1,16 @@
 package inno.jago.ast.expression.unary_expression.primary_expression.operand.literal_operand
 
 import inno.jago.ast.type.TypeNode
+import inno.jago.entity.Entity
+import inno.jago.lexer.Pos
 
-sealed class LiteralTypeNode
+sealed class LiteralTypeNode(
+    pos: Pos
+) : Entity(pos)
 
 class ArrayTypeNodeNode(
+    pos: Pos,
     val length: Int,
     val type: TypeNode
-) : LiteralTypeNode()
+) : LiteralTypeNode(pos)
 
