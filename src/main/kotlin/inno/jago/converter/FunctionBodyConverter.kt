cefa4ef85a973f3cb4014b76e632d8512cb0fa44
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-04-11 20:55:53 +0300

    block statement

--- src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
+++ src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
@@ -5,6 +5,7 @@ import inno.jago.ast.statement.BlockStatementNode
 import inno.jago.ast.statement.StatementNode
 import inno.jago.converter.statement.simple_statement.toSimpleStatementNode
 import inno.jago.converter.statement.toDeclarationStatementNode
+import inno.jago.converter.statement.toBlockStatementNode
 
 fun GoParser.FunctionBodyContext.toBlockStatementNode(): BlockStatementNode {
     val statementNodes = mutableListOf<StatementNode>()
@@ -14,7 +15,7 @@ fun GoParser.FunctionBodyContext.toBlockStatementNode(): BlockStatementNode {
                 statementNodes.add(it.toSimpleStatementNode())
             }
             block()?.let {
-                TODO()
+                statementNodes.add(it.toBlockStatementNode())
             }
             declaration()?.let {
                 it.toDeclarationStatementNode().let { declarationStatementNode ->
--- src/main/kotlin/inno/jago/converter/statement/BlockConverters.kt
+++ src/main/kotlin/inno/jago/converter/statement/BlockConverters.kt
@@ -1,2 +1,50 @@
 package inno.jago.converter.statement
 
+import GoParser
+import inno.jago.ast.statement.BlockStatementNode
+import inno.jago.ast.statement.StatementNode
+import inno.jago.converter.statement.simple_statement.toSimpleStatementNode
+import inno.jago.converter.toPos
+
+fun GoParser.BlockContext.toBlockStatementNode(): BlockStatementNode {
+    val statementNodes = mutableListOf<StatementNode>()
+
+    statementList().statement().forEach { statement ->
+        statementNodes.add(statement.toStatementNode())
+    }
+
+    return BlockStatementNode(toPos(), statementNodes)
+}
+
+fun GoParser.StatementContext.toStatementNode(): StatementNode {
+    val statementNodes = mutableListOf<StatementNode>()
+    block().statementList().statement().forEach { statement ->
+        with(statement) {
+            simpleStmt()?.let {
+                statementNodes.add(it.toSimpleStatementNode())
+            }
+            block()?.let {
+                statementNodes.add(it.toBlockStatementNode())
+            }
+            declaration()?.let {
+                TODO()
+            }
+            returnStmt()?.let {
+                TODO()
+            }
+            breakStmt()?.let {
+                TODO()
+            }
+            continueStmt()?.let {
+                TODO()
+            }
+            ifStmt()?.let {
+                TODO()
+            }
+            forStmt()?.let {
+                TODO()
+            }
+        }
+    }
+    return BlockStatementNode(toPos(), statementNodes)
+}
