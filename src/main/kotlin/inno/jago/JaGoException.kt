fbfc09c74e8adf0e38587787de6c6a9cdd0d5819
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-04-11 22:59:58 +0300

    add binary exp

--- src/main/kotlin/inno/jago/JaGoException.kt
+++ src/main/kotlin/inno/jago/JaGoException.kt
@@ -8,6 +8,8 @@ sealed class JaGoException(msg: String) : Exception(msg)
 class WrongNumberOfExpressions(expected: Int, actual: Int) : JaGoException(
     "Expected $expected or func expressions, got $actual expression"
 )
+
+class WrongBinaryExpressionNumber() : JaGoException("Expected two expressions around binary operator")
 class ConversionException(msg: String) : JaGoException(msg)
 
 class EntityNotSupported(entityName: String) : JaGoException("$entityName are not supported")
