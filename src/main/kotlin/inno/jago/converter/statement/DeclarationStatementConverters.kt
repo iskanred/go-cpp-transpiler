6168eb7231a15fd79307c174d37745a81f18d125
Vagif Khalilov <lebronirish@gmail.com>
2023-04-11 20:38:22 +0300

    implement DeclContext -> DeclarationStatementNode converter

--- src/main/kotlin/inno/jago/converter/statement/DeclarationStatementConverters.kt
+++ src/main/kotlin/inno/jago/converter/statement/DeclarationStatementConverters.kt
@@ -1,2 +1,13 @@
 package inno.jago.converter.statement
 
+import inno.jago.ast.statement.BlockStatementNode
+import inno.jago.ast.statement.DeclarationStatementNode
+import inno.jago.converter.toDeclarationNodes
+import inno.jago.converter.toPos
+import inno.jago.lexer.Pos
+fun GoParser.DeclarationContext.toDeclarationStatementNode(): DeclarationStatementNode{
+    return DeclarationStatementNode(
+        pos = toPos(),
+        declaration = toDeclarationNodes()[0]
+    )
+}
