bff07fbd3b7b450b8476434c1dd2e644f4cccb6b
iskanred <nafikov.iskander@mail.ru>
2023-04-11 19:37:41 +0300

    feat: add signature converter

---
+++ src/main/kotlin/inno/jago/converter/SignatureConverter.kt
@@ -0,0 +1,16 @@
+package inno.jago.converter
+
+import GoParser
+import inno.jago.ast.signature.ParameterNode
+import inno.jago.ast.signature.SignatureNode
+
+fun GoParser.SignatureContext.toSignatureNode() = SignatureNode(
+    parameterNodes = parameters().toParameterNodes(),
+    type = TODO(),
+)
+
+fun GoParser.ParametersContext.toParameterNodes(): List<ParameterNode> {
+    parameterList().parameterDecl().forEach { paramDecl ->
+        paramDecl
+    }
+}
