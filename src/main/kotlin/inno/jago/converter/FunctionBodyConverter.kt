3db9c69e1bd90fb4842b49d803ca06032de5b7ba
Vagif Khalilov <lebronirish@gmail.com>
2023-04-11 20:45:09 +0300

    vagif pushnul declaraciou

--- src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
+++ src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
@@ -4,6 +4,7 @@ import GoParser
 import inno.jago.ast.statement.BlockStatementNode
 import inno.jago.ast.statement.StatementNode
 import inno.jago.converter.statement.simple_statement.toSimpleStatementNode
+import inno.jago.converter.statement.toDeclarationStatementNode
 
 fun GoParser.FunctionBodyContext.toBlockStatementNode(): BlockStatementNode {
     val statementNodes = mutableListOf<StatementNode>()
@@ -16,7 +17,9 @@ fun GoParser.FunctionBodyContext.toBlockStatementNode(): BlockStatementNode {
                 TODO()
             }
             declaration()?.let {
-                TODO()
+                it.toDeclarationStatementNode().let { declarationStatementNode ->
+                    statementNodes.add(declarationStatementNode)
+                }
             }
             returnStmt()?.let {
                 TODO()
