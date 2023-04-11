6ed2dfc55fd9b5c1359144caf0d62413ceee4fd8
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-04-11 21:56:14 +0300

    return_final

--- src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
+++ src/main/kotlin/inno/jago/converter/FunctionBodyConverter.kt
@@ -5,10 +5,10 @@ import inno.jago.ast.expression.ExpressionNode
 import inno.jago.ast.statement.BlockStatementNode
 import inno.jago.ast.statement.ReturnStatementNode
 import inno.jago.ast.statement.StatementNode
+import inno.jago.converter.expression.toExpressionNode
 import inno.jago.converter.statement.simple_statement.toSimpleStatementNode
 import inno.jago.converter.statement.toDeclarationStatementNode
 import inno.jago.converter.statement.toBlockStatementNode
-import java.beans.Expression
 import inno.jago.converter.statement.toBreakStatementNode
 import inno.jago.converter.statement.toContinueStatementNode
 
@@ -49,7 +49,7 @@ fun GoParser.ReturnStmtContext.toReturnStatementNode() : ReturnStatementNode {
     val expressionNodes = mutableListOf<ExpressionNode>()
 
     expressionList()?.expression()?.forEach {
-        expressionNodes.add(it.toExpressionNode)
+        expressionNodes.add(it.toExpressionNode())
     }
 
     return ReturnStatementNode(toPos(), expressionNodes)
