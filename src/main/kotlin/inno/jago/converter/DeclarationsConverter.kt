40784136d84d2f32493fc8f22e2e3f7b77d70b38
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-04-11 19:37:33 +0300

    add var spec

--- src/main/kotlin/inno/jago/converter/DeclarationsConverter.kt
+++ src/main/kotlin/inno/jago/converter/DeclarationsConverter.kt
@@ -4,24 +4,50 @@ import inno.jago.EntityNotSupported
 import inno.jago.UnreachableCodeException
 import inno.jago.ast.decl.DeclarationNode
 import inno.jago.ast.decl.FunctionDeclarationNode
+import inno.jago.ast.decl.VarDeclarationNode
+import inno.jago.ast.type.TypeNode
 import inno.jago.lexer.Pos
+import org.antlr.v4.runtime.ParserRuleContext
 
 fun GoParser.DeclarationContext.toDeclarationNodes(): List<DeclarationNode> {
     constDecl()?.let {
         return TODO()
     }
+
     varDecl()?.let {
-        return TODO()
+        it.varSpec().forEach {
+            it.toVarDeclarationNodes()
+        }
     }
+
     typeDecl()?.let {
         throw EntityNotSupported("Types")
     }
     throw UnreachableCodeException()
 }
 
-fun GoParser.FunctionDeclContext.toFunctionDeclarationNode() = FunctionDeclarationNode(
-    pos = Pos(tokenStart = start, tokenStop = stop),
-    functionName = functionName().IDENTIFIER().text,
-    signature = TODO(),
-    functionBody = TODO()
-)
+fun GoParser.VarSpecContext.toVarDeclarationNodes(): List<VarDeclarationNode> {
+    var ids = identifierList().IDENTIFIER()
+    var exps = expressionList().expression()
+
+}
+
+fun GoParser.TypeContext.toTypeNode(): TypeNode {
+    return TypeNode(
+        pos = toPos(),
+        name = typeName().IDENTIFIER().text
+    )
+}
+
+fun GoParser.FunctionDeclContext.toFunctionDeclarationNode(): FunctionDeclarationNode {
+    return FunctionDeclarationNode(
+        pos = toPos(),
+        functionName = functionName().IDENTIFIER().text,
+        signature = TODO(),
+        functionBody = TODO()
+    )
+}
+
+fun ParserRuleContext.toPos(): Pos {
+    return Pos(tokenStart = start, tokenStop = stop)
+}
