92e651868a84a6aa2ea05a6fc5b8febbae6cd535
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-04-30 20:30:08 +0300

    IndexExpressionNode

--- src/main/kotlin/inno/jago/semantic/analyzer/expression/UnaryOrPrimaryExpressionAnalyzer.kt
+++ src/main/kotlin/inno/jago/semantic/analyzer/expression/UnaryOrPrimaryExpressionAnalyzer.kt
@@ -12,9 +12,14 @@ import inno.jago.ast.model.expression.unary_expression.primary_expression.operan
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.LiteralOperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNameNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNode
+import inno.jago.ast.model.type.ArrayTypeNode
 import inno.jago.exception.UnreachableCodeException
+import inno.jago.semantic.NoSuchVariableInCurrentScopeException
+import inno.jago.semantic.WrongTypeException
+import inno.jago.semantic.model.EntityType
 import inno.jago.semantic.model.ScopeNode
 import inno.jago.semantic.model.SemanticEntity
+import inno.jago.semantic.model.Type
 import java.beans.Expression
 
 fun UnaryOrPrimaryExpression.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
@@ -28,9 +33,9 @@ fun UnaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
 
 @Suppress("CyclomaticComplexMethod")
 fun PrimaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
-    is ExpressionOperandNode -> TODO()
+    is ExpressionOperandNode -> toSemanticEntity(scope)
     is ConversionNode -> TODO()
-    is IndexExpressionNode -> TODO()
+    is IndexExpressionNode -> toSemanticEntity(scope)
     is SelectorExpressionNode -> TODO()
     is ApplicationExpressionNode -> TODO()
     // делает iskanred
@@ -40,7 +45,28 @@ fun PrimaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = w
     else -> throw UnreachableCodeException()
 }
 
-fun ExpressionOperandNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
-    return expression.toSemanticEntity(scope)
-}
+fun IndexExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity  {
+    val expr = primaryExpression.toSemanticEntity(scope)
+    val index = expression.toSemanticEntity(scope)
 
+    if (index.type !is Type.IntegerType) {
+        throw WrongTypeException(Type.IntegerType, index)
+    }
+
+    if (expr.identifier != null) {
+        val exprFromScope = scope.findVisibleEntity(expr.identifier)
+        if (exprFromScope != null) {
+            if (exprFromScope.type != expr.type) {
+                throw WrongTypeException(Type.ArrayType(-1, Type.AnyType), index)
+            }
+        } else {
+            throw NoSuchVariableInCurrentScopeException(expr.identifier, pos)
+        }
+    }
+
+    if (expr.type !is Type.ArrayType) {
+        throw WrongTypeException(Type.ArrayType(-1, Type.AnyType), index)
+    }
+
+    return SemanticEntity(expr.type, pos, EntityType.EXPRESSION)
+}
