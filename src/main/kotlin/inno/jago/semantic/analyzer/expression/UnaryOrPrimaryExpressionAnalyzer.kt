cf69c147f8c7c7edd0202673df9155f851f3369f
Vagif Khalilov <lebronirish@gmail.com>
2023-04-30 19:56:39 +0300

    add ConversionNode

--- src/main/kotlin/inno/jago/semantic/analyzer/expression/UnaryOrPrimaryExpressionAnalyzer.kt
+++ src/main/kotlin/inno/jago/semantic/analyzer/expression/UnaryOrPrimaryExpressionAnalyzer.kt
@@ -12,8 +12,12 @@ import inno.jago.ast.model.expression.unary_expression.primary_expression.operan
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNameNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNode
 import inno.jago.exception.UnreachableCodeException
+import inno.jago.semantic.NonCastableTypeException
+import inno.jago.semantic.model.EntityType
 import inno.jago.semantic.model.ScopeNode
 import inno.jago.semantic.model.SemanticEntity
+import inno.jago.semantic.model.Type
+import inno.jago.semantic.model.toType
 
 fun UnaryOrPrimaryExpression.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
     is UnaryExpressionNode -> toSemanticEntity(scope)
@@ -27,7 +31,7 @@ fun UnaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
 @Suppress("CyclomaticComplexMethod")
 fun PrimaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
     is ExpressionOperandNode -> TODO()
-    is ConversionNode -> TODO()
+    is ConversionNode -> toSemanticEntity(scope)
     is IndexExpressionNode -> TODO()
     is SelectorExpressionNode -> TODO()
     is ApplicationExpressionNode -> TODO()
@@ -36,4 +40,19 @@ fun PrimaryExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = w
     is OperandNameNode -> toSemanticEntity(scope)
     is OperandNode -> TODO()
     else -> throw UnreachableCodeException()
+}
+
+fun ConversionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
+    var entity = expression.toSemanticEntity(scope)
+    if (entity.type != type.toType() ) {
+        if (entity.type !is Type.NumberType || type.toType() !is Type.NumberType) {
+            throw NonCastableTypeException(entity.type, type.toType(), pos)
+        }
+        return SemanticEntity(
+            type = type.toType(),
+            pos = pos,
+            entityType = EntityType.EXPRESSION
+        )
+    }
+    return entity
 }
