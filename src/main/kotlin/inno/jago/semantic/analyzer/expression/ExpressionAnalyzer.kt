8e585161ef6a0381dd1afcc6d0b828d2d1dd0f69
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-04-30 18:52:56 +0300

    add when to expression semantic analyzer

--- src/main/kotlin/inno/jago/semantic/analyzer/expression/ExpressionAnalyzer.kt
+++ src/main/kotlin/inno/jago/semantic/analyzer/expression/ExpressionAnalyzer.kt
@@ -2,9 +2,18 @@ package inno.jago.semantic.analyzer.expression
 
 import inno.jago.ast.model.expression.ExpressionNode
 import inno.jago.ast.model.expression.binary_expression.BinaryExpression
+import inno.jago.ast.model.expression.unary_expression.ApplicationExpressionNode
+import inno.jago.ast.model.expression.unary_expression.ConversionNode
+import inno.jago.ast.model.expression.unary_expression.IndexExpressionNode
+import inno.jago.ast.model.expression.unary_expression.PrimaryExpressionNode
+import inno.jago.ast.model.expression.unary_expression.SelectorExpressionNode
+import inno.jago.ast.model.expression.unary_expression.UnaryExpressionNode
+import inno.jago.ast.model.expression.unary_expression.UnaryOperatorNode
 import inno.jago.ast.model.expression.unary_expression.UnaryOrPrimaryExpression
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.ExpressionOperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.LiteralOperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNameNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.QualifiedIdentifierOperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.SimpleIdentifierOperandNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.IntegerLiteralNode
@@ -15,29 +24,74 @@ import inno.jago.semantic.model.ScopeNode
 import inno.jago.semantic.model.SemanticEntity
 import inno.jago.semantic.model.Type
 
-fun ExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = when(this) {
+fun ExpressionNode.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
     is UnaryOrPrimaryExpression -> toSemanticEntity(scope)
     is BinaryExpression -> TODO()
     else -> throw UnreachableCodeException()
 }
 
-fun UnaryOrPrimaryExpression.toSemanticEntity(scope: ScopeNode): SemanticEntity = when(this) {
-    is OperandNameNode -> {
-        when (name) {
-            is QualifiedIdentifierOperandNode -> {
-                scope.findVisibleEntity(name.qualifiedIdentifier.packageName) ?: throw NoSuchVariableInCurrentScopeException(name.qualifiedIdentifier.packageName, pos)
+fun UnaryOrPrimaryExpression.toSemanticEntity(scope: ScopeNode): SemanticEntity = when (this) {
+
+    is UnaryExpressionNode -> {
+        throw NotImplementedError()
+    }
+
+    is PrimaryExpressionNode -> {
+        when (this) {
+            is ExpressionOperandNode -> {
+                throw NotImplementedError()
             }
-            is SimpleIdentifierOperandNode -> {
-                scope.findVisibleEntity(name.identifier) ?: throw NoSuchVariableInCurrentScopeException(name.identifier, pos)
+
+            is ConversionNode -> {
+                throw NotImplementedError()
+            }
+
+            is IndexExpressionNode -> {
+                throw NotImplementedError()
+            }
+
+            is SelectorExpressionNode -> {
+                throw NotImplementedError()
+            }
+
+            is ApplicationExpressionNode -> {
+                throw NotImplementedError()
+            }
+
+            is LiteralOperandNode -> {
+                when (literalNode) {
+                    is IntegerLiteralNode -> SemanticEntity(Type.IntegerType, pos, EntityType.EXPRESSION, null)
+                    else -> throw NotImplementedError()
+                }
+            }
+
+            is OperandNameNode -> {
+                when (name) {
+                    is QualifiedIdentifierOperandNode -> {
+                        scope.findVisibleEntity(name.qualifiedIdentifier.packageName)
+                            ?: throw NoSuchVariableInCurrentScopeException(name.qualifiedIdentifier.packageName, pos)
+                    }
+
+                    is SimpleIdentifierOperandNode -> {
+                        scope.findVisibleEntity(name.identifier) ?: throw NoSuchVariableInCurrentScopeException(
+                            name.identifier,
+                            pos
+                        )
+                    }
+                }
+            }
+
+            is OperandNode -> {
+                throw NotImplementedError()
+            }
+
+            else -> {
+                throw NotImplementedError()
             }
         }
     }
-    is LiteralOperandNode -> {
-        when (literalNode) {
-            is IntegerLiteralNode -> SemanticEntity(Type.IntegerType, pos, EntityType.EXPRESSION, null)
-            else -> throw NotImplementedError()
-        }
-    }
+
+
     else -> throw NotImplementedError()
 }
 
