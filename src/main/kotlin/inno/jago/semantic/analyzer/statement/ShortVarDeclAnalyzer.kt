3e310e1ef0763ad4d791b60a8dc2ec2365ea009d
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-01 20:03:56 +0300

    implement shortVarDecl analyzer

--- src/main/kotlin/inno/jago/semantic/analyzer/statement/ShortVarDeclAnalyzer.kt
+++ src/main/kotlin/inno/jago/semantic/analyzer/statement/ShortVarDeclAnalyzer.kt
@@ -4,60 +4,81 @@ import inno.jago.ast.model.statement.ShortVarDeclNode
 import inno.jago.common.JaGoException
 import inno.jago.common.UnreachableCodeException
 import inno.jago.common.WrongNumberOfExpressionsException
+import inno.jago.semantic.WrongTypeException
 import inno.jago.semantic.model.EntityType
 import inno.jago.semantic.model.ScopeNode
 import inno.jago.semantic.model.Type
 import inno.jago.semantic.model.SemanticEntity
 
 // ShortVarDecl = IdentifierList ":=" ExpressionList
-@Suppress("ThrowsCount")
 fun ShortVarDeclNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
     val semanticEntities = expression.map { toSemanticEntity(scope) }
-    if (semanticEntities.any { it.type is Type.FuncType } ) {
+    if (semanticEntities.any { it.type is Type.TupleType } ) { // a, b := someFun(), someFun return > 1 elements
         if (semanticEntities.size != 1) {
-            throw JaGoException(msg = "Can be only one function in expressions")
+            throw JaGoException(msg = "Can be only one tuple in expressions")
         }
 
-        val funcEntity = semanticEntities.first()
-        if (funcEntity.type !is Type.FuncType) {
+        val tupleEntity = semanticEntities.first()
+        if (tupleEntity.type !is Type.TupleType) {
             throw UnreachableCodeException()
         }
 
-        val returnType = funcEntity.type.returnType
-        if (returnType is Type.TupleType) {
-            if (identifierList.size != returnType.elementTypes.size) {
-                throw WrongNumberOfExpressionsException(
-                    expected =  identifierList.size,
-                    actual = returnType.elementTypes.size,
-                    pos = pos
-                )
-            }
-
-        } else if (returnType is Type.UnitType) {
-            throw JaGoException("Function return nothing in short var decl")
-        } else {
-            if (identifierList.size != 1) {
-                throw WrongNumberOfExpressionsException(
-                    expected = identifierList.size,
-                    actual = 1,
-                    pos = pos
-                )
-            }
-
-            val entity = SemanticEntity(
-                type = returnType, pos = pos,
-                entityType = EntityType.VAR, identifier = identifierList.first()
+        val tupleElements = tupleEntity.type.elementTypes
+        if (identifierList.size != tupleElements.size) {
+            throw WrongNumberOfExpressionsException(
+                expected =  identifierList.size,
+                actual = tupleElements.size,
+                pos = pos
             )
+        }
 
-            scope.addUniqueEntity(entity)
-            return entity
+        identifierList.zip(tupleElements).forEach {
+            val visibleEntity = scope.findVisibleEntity(it.first)
+            if (visibleEntity == null) {
+                scope.addUniqueEntity(SemanticEntity(
+                    type = it.second,
+                    pos = pos,
+                    entityType = EntityType.VAR,
+                    identifier = it.first
+                ))
+                return@forEach
+            }
+
+            if (visibleEntity.type != it.second) {
+                throw WrongTypeException(it.second, actual = visibleEntity)
+            }
         }
     }
-    else {
+    else { // a, b := 3, true. No tuple in semanticEntities.
         if (semanticEntities.size != identifierList.size) {
-            throw TODO()
+            throw WrongNumberOfExpressionsException(
+                expected =  identifierList.size,
+                actual = semanticEntities.size,
+                pos = pos
+            )
         }
 
+        identifierList.zip(semanticEntities).forEach {
+            val visibleEntity = scope.findVisibleEntity(it.first)
+            if (visibleEntity == null) {
+                scope.addUniqueEntity(SemanticEntity(
+                    type = it.second.type,
+                    pos = pos,
+                    entityType = EntityType.VAR,
+                    identifier = it.first
+                ))
+                return@forEach
+            }
+
+            if (visibleEntity.type != it.second.type) {
+                throw WrongTypeException(it.second.type, actual = visibleEntity)
+            }
+        }
     }
 
+    return SemanticEntity(
+        type = Type.UnitType,
+        pos = pos,
+        entityType = EntityType.NO_IDENTIFIER
+    )
 }
