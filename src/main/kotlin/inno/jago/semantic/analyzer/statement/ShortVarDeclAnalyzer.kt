84210ec685a997f3e784d2db21307220c351af4f
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-01 16:29:40 +0300

    add short var decl wrong impl

--- src/main/kotlin/inno/jago/semantic/analyzer/statement/ShortVarDeclAnalyzer.kt
+++ src/main/kotlin/inno/jago/semantic/analyzer/statement/ShortVarDeclAnalyzer.kt
@@ -1,9 +1,63 @@
 package inno.jago.semantic.analyzer.statement
 
 import inno.jago.ast.model.statement.ShortVarDeclNode
+import inno.jago.common.JaGoException
+import inno.jago.common.UnreachableCodeException
+import inno.jago.common.WrongNumberOfExpressionsException
+import inno.jago.semantic.SemanticException
+import inno.jago.semantic.model.EntityType
 import inno.jago.semantic.model.ScopeNode
+import inno.jago.semantic.model.Type
 import inno.jago.semantic.model.SemanticEntity
 
+// ShortVarDecl = IdentifierList ":=" ExpressionList
 fun ShortVarDeclNode.toSemanticEntity(scope: ScopeNode): SemanticEntity {
-    TODO()
+    val semanticEntities = expression.map { toSemanticEntity(scope) }
+    if (semanticEntities.any { it.type is Type.FuncType } ) {
+        if (semanticEntities.size != 1) {
+            throw JaGoException(msg = "Can be only one function in expressions")
+        }
+
+        val funcEntity = semanticEntities.first()
+        if (funcEntity.type !is Type.FuncType) {
+            throw UnreachableCodeException()
+        }
+
+        val returnType = funcEntity.type.returnType
+        if (returnType is Type.TupleType) {
+            if (identifierList.size != returnType.elementTypes.size) {
+                throw WrongNumberOfExpressionsException(
+                    expected =  identifierList.size,
+                    actual = returnType.elementTypes.size,
+                    pos = pos
+                )
+            }
+
+        } else if (returnType is Type.UnitType) {
+            throw JaGoException("Function return nothing in short var decl")
+        } else {
+            if (identifierList.size != 1) {
+                throw WrongNumberOfExpressionsException(
+                    expected = identifierList.size,
+                    actual = 1,
+                    pos = pos
+                )
+            }
+
+            val entity = SemanticEntity(
+                type = returnType, pos = pos,
+                entityType = EntityType.VARIABLE, identifier = identifierList.first()
+            )
+
+            scope.addUniqueEntity(entity)
+            return entity
+        }
+    }
+    else {
+        if (semanticEntities.size != identifierList.size) {
+            throw TODO()
+        }
+
+    }
+
 }
