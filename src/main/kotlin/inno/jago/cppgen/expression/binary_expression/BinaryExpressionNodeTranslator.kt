3aafdb3cc2c1262a98ffd4e9a28b39fcce94d4d3
e.zaripov <e.zaripov@tinkoff.ru>
2023-05-03 00:14:33 +0300

    unary or primary translator skeleton

--- src/main/kotlin/inno/jago/cppgen/expression/binary_expression/BinaryExpressionNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/expression/binary_expression/BinaryExpressionNodeTranslator.kt
@@ -3,5 +3,5 @@ package inno.jago.cppgen.expression.binary_expression
 import inno.jago.ast.model.expression.binary_expression.BinaryExpression
 
 fun BinaryExpression.translateToCode(): String {
-    TODO()
+
 }
---
+++ src/main/kotlin/inno/jago/cppgen/expression/unary_or_primary_expression/UnaryExpressionNodeTranslator.kt
@@ -0,0 +1,7 @@
+package inno.jago.cppgen.expression.unary_or_primary_expression
+
+import inno.jago.ast.model.expression.unary_expression.UnaryExpressionNode
+
+fun UnaryExpressionNode.translateToCode(): String {
+
+}
