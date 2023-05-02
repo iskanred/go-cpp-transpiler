94af2c0640b4ba24717aae7bee48c80689261281
Vagif Khalilov <lebronirish@gmail.com>
2023-05-02 23:11:01 +0300

    add: translateToCode for ReturnStatementNode

--- src/main/kotlin/inno/jago/cppgen/statement/ReturnStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/ReturnStatementNodeTranslator.kt
@@ -1,7 +1,25 @@
 package inno.jago.cppgen.statement
 
 import inno.jago.ast.model.statement.ReturnStatementNode
+import inno.jago.cppgen.expression.translateToCode
 
 fun ReturnStatementNode.translateToCode(): String {
-    TODO()
+    if (expressions.isEmpty()) {
+        return "return;"
+    }
+    var returnInstruction = "return "
+
+    if (expressions.size == 1) {
+        returnInstruction += expressions[0].translateToCode() + ";"
+    } else {
+        returnInstruction += "make_tuple("
+        for (i in 0 until expressions.size) {
+            returnInstruction += expressions[i].translateToCode()
+            if (i != expressions.size - 1) {
+                returnInstruction += ", "
+            }
+        }
+        returnInstruction += ");"
+    }
+    return returnInstruction
 }
