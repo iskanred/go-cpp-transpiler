7be7d03a9d9324621fce03a0a5bd4caed7250a8d
e.zaripov <e.zaripov@tinkoff.ru>
2023-05-03 00:14:33 +0300

    unary or primary translator skeleton

---
+++ src/main/kotlin/inno/jago/cppgen/expression/unary_or_primary_expression/PrimaryExpressionNodeTranslator.kt
@@ -0,0 +1,12 @@
+package inno.jago.cppgen.expression.unary_or_primary_expression
+
+import inno.jago.ast.model.expression.unary_expression.ApplicationExpressionNode
+import inno.jago.ast.model.expression.unary_expression.PrimaryExpressionNode
+
+
+fun PrimaryExpressionNode.translateToCode(): String = when (this) {
+    is ApplicationExpressionNode -> TODO()
+
+}
+
+
