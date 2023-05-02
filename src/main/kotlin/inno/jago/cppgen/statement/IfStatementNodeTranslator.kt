e9165621c8742f66e50bf736c4ce7019d7df5f60
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-03 02:33:34 +0300

    complete simple statement codegne

--- src/main/kotlin/inno/jago/cppgen/statement/IfStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/IfStatementNodeTranslator.kt
@@ -4,6 +4,8 @@ import inno.jago.ast.model.statement.IfStatementNode
 import inno.jago.cppgen.expression.translateToCode
 
 fun IfStatementNode.translateToCode(): String {
+    // TODO: Возможно, стоит менять название переменной на UUID
+    //  для уникальности переменных в области видимости
     var simpleStatementCode = ""
     if (simpleStatement != null) {
         simpleStatementCode = simpleStatement!!.translateToCode() + "; "
--- src/main/kotlin/inno/jago/cppgen/statement/SimpleStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/SimpleStatementNodeTranslator.kt
@@ -1,11 +1,15 @@
 package inno.jago.cppgen.statement
 
+import inno.jago.ast.model.statement.AddOpSimpleAssignOperatorNode
 import inno.jago.ast.model.statement.AssignmentNode
 import inno.jago.ast.model.statement.EmptyStatementNode
 import inno.jago.ast.model.statement.ExpressionStatementNode
 import inno.jago.ast.model.statement.IncDecStatementNode
+import inno.jago.ast.model.statement.MulOpSimpleAssignOperatorNode
 import inno.jago.ast.model.statement.ShortVarDeclNode
+import inno.jago.ast.model.statement.SimpleAssignOperatorNode
 import inno.jago.ast.model.statement.SimpleStatementNode
+import inno.jago.cppgen.expression.binary_expression.translateToCode
 import inno.jago.cppgen.expression.translateToCode
 
 fun SimpleStatementNode.translateToCode(): String = when (this) {
@@ -17,7 +21,23 @@ fun SimpleStatementNode.translateToCode(): String = when (this) {
 }
 
 fun AssignmentNode.translateToCode(): String {
+    if (leftExpressions.size != rightExpressions.size) {
+        TODO("Сделать для кортежей")
+    }
 
+    leftExpressions.map { translateToCode() }
+    rightExpressions.map { translateToCode() }
+
+    var res = ""
+    leftExpressions.zip(rightExpressions).forEach { (lhs, rhs) ->
+        res += when (val it = assignOperator) {
+            is SimpleAssignOperatorNode -> "$lhs = $rhs; "
+            is AddOpSimpleAssignOperatorNode -> "$lhs ${it.addOperator.translateToCode()}= $rhs; "
+            is MulOpSimpleAssignOperatorNode -> "$lhs ${it.mulOperator.translateToCode()}= $rhs; "
+        }
+    }
+
+    return res
 }
 
 fun IncDecStatementNode.translateToCode(): String = when (type) {
@@ -26,6 +46,17 @@ fun IncDecStatementNode.translateToCode(): String = when (type) {
 }
 
 fun ShortVarDeclNode.translateToCode(): String {
+    if (identifierList.size != expression.size) {
+        TODO("Сделать для кортежей")
+    }
 
+    var res = ""
+    identifierList.zip(expression.map { translateToCode() }).forEach { (identifier, expressionCode) ->
+        if (identifier != "_") {
+            res += "auto $identifier = $expressionCode; "
+        }
+    }
+
+    return res
 }
 
