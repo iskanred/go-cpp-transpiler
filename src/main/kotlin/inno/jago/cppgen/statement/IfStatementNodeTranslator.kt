4c2a378d86fe23252d7667d779d9afe06c43ce6f
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-03 00:02:24 +0300

    add if codegen

--- src/main/kotlin/inno/jago/cppgen/statement/IfStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/IfStatementNodeTranslator.kt
@@ -1,8 +1,14 @@
 package inno.jago.cppgen.statement
 
 import inno.jago.ast.model.statement.IfStatementNode
+import inno.jago.cppgen.expression.translateToCode
 
 fun IfStatementNode.translateToCode(): String {
-    TODO()
+    var simpleStatementCode = ""
+    if (simpleStatement != null) {
+        simpleStatementCode = simpleStatement!!.translateToCode() + "; "
+    }
+
+    return simpleStatementCode + "if (${expression.translateToCode()}) ${block.translateToCode()} ${elseBranch?.translateToCode() ?: ""}"
 }
 
