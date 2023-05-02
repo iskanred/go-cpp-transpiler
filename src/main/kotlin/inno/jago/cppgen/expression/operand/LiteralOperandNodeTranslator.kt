e86c25a246801be4f404b07e9d03b541a153fba1
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-05-03 02:18:32 +0300

    LiteralOperandNode

--- src/main/kotlin/inno/jago/cppgen/expression/operand/LiteralOperandNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/expression/operand/LiteralOperandNodeTranslator.kt
@@ -1,8 +1,34 @@
 package inno.jago.cppgen.expression.operand
 
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.LiteralOperandNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.BasicLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.BoolLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.CompositeLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.DoubleLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.FunctionLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.IntegerLiteralNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.literal_operand.StringLiteralNode
+import inno.jago.cppgen.expression.translateToCode
+import inno.jago.cppgen.type.translateToCode
 
 
 fun LiteralOperandNode.translateToCode(): String {
-    TODO()
+    when (val it = literalNode) {
+        is IntegerLiteralNode -> {
+            return it.intValue.toString()
+        }
+        is BoolLiteralNode -> {
+            return it.boolValue.toString()
+        }
+        is DoubleLiteralNode -> {
+            return it.doubleValue.toString()
+        }
+        is StringLiteralNode -> {
+            return "\"" + it.toString() + "\""
+        }
+        is CompositeLiteralNode -> {
+            return it.literal.elementType.translateToCode() + "[" + it.literal.length.translateToCode() + "]{" + it.literalValue.elements.map { elem -> elem.translateToCode() } + "}";
+        }
+        is FunctionLiteralNode -> TODO()
+    }
 }
