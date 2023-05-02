e68126925a915db733171adee472a221b8d36da6
Vagif Khalilov <lebronirish@gmail.com>
2023-05-03 01:11:33 +0300

    add: translateToCode function for OperandNode

--- src/main/kotlin/inno/jago/cppgen/expression/operand/OperandNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/expression/operand/OperandNodeTranslator.kt
@@ -1,7 +1,28 @@
 package inno.jago.cppgen.expression.operand
 
+import inno.jago.ast.model.expression.unary_expression.ApplicationExpressionNode
+import inno.jago.ast.model.expression.unary_expression.ConversionNode
+import inno.jago.ast.model.expression.unary_expression.IndexExpressionNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.ExpressionOperandNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.LiteralOperandNode
+import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNameNode
 import inno.jago.ast.model.expression.unary_expression.primary_expression.operand.OperandNode
+import inno.jago.common.UnreachableCodeException
 
-fun OperandNode.translateToCode(): String {
+fun OperandNode.translateToCode(): String = when (this) {
+    is LiteralOperandNode -> translateToCode()
+    is OperandNameNode -> translateToCode()
+    is ExpressionOperandNode -> translateToCode()
+}
+
+fun LiteralOperandNode.translateToCode(): String {
+    TODO()
+}
+
+fun OperandNameNode.translateToCode(): String {
+    TODO()
+}
+
+fun ExpressionOperandNode.translateToCode(): String {
     TODO()
 }
