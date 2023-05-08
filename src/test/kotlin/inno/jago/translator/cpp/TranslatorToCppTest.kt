526c5a1044f3bf9d30b422e10ae804a37e0c5717
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2023-05-08 21:50:34 +0300

    comment test

--- src/test/kotlin/inno/jago/translator/cpp/TranslatorToCppTest.kt
+++ src/test/kotlin/inno/jago/translator/cpp/TranslatorToCppTest.kt
@@ -5,21 +5,20 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.stream.Stream
-import
-class TranslatorToCppTest {
-
-    @ParamerizedTest
-    fun `hello world`() {
-
-
-    }
-
-    companion object {
-        private const val BASE_DIR = "src/test/resources/translator/input"
-
-        @JvmStatic
-        fun pathStream(): Stream<Path> = getFilesStream(BASE_DIR)
-
-        private fun getFilesStream(path: String) = Files.list(Paths.get(path))
-    }
-}
+//class TranslatorToCppTest {
+//
+//    @ParamerizedTest
+//    fun `hello world`() {
+//
+//
+//    }
+//
+//    companion object {
+//        private const val BASE_DIR = "src/test/resources/translator/input"
+//
+//        @JvmStatic
+//        fun pathStream(): Stream<Path> = getFilesStream(BASE_DIR)
+//
+//        private fun getFilesStream(path: String) = Files.list(Paths.get(path))
+//    }
+//}
