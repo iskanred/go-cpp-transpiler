627f2b4ed7b6a1dda5c6352f6109da718b89538e
Vagif Khalilov <lebronirish@gmail.com>
2023-05-08 21:23:32 +0300

    add test for var declaration

---
+++ src/test/resources/tests/type_checker/variable/invalid_var_declaration_2.go
@@ -0,0 +1,9 @@
+package main
+
+func getInt() int {
+    return 1
+}
+
+func main(){
+    var a string = getInt()
+}
---
+++ src/test/resources/tests/type_checker/variable/invalid_var_declaration_3.go
@@ -0,0 +1,9 @@
+package main
+
+func someFunc() (int, int) {
+    return 1, 2
+}
+
+func main(){
+    var a int = someFunc()
+}
