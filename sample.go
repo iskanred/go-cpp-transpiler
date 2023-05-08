9b80ea495aa6a6d9b961a4cdd4e3a07a3c382e3f
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-08 17:57:52 +0300

    add tuples for assignment

--- sample.go
+++ sample.go
@@ -1,22 +1,11 @@
-package name;
-func find(a int) (string) {
-    //ints := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    strs := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+package name
 
-    if (a >= 0) && (a <= 9) {
-        return strs[a];
-    };
+func good1() (string, int) {
+    return "123", 23
+}
 
-    return "x";
-};
-func good1(a int) (string) {
-    var b string;
-    for a > 0 {
-        b += find(a % 10);
-        a /= 10;
-    };
-    return b;
-};
 func main() {
-	good1()
+    var a string
+    var b int
+    a, b = good1()
 }
--- src/main/kotlin/inno/jago/cppgen/statement/SimpleStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/SimpleStatementNodeTranslator.kt
@@ -11,6 +11,9 @@ import inno.jago.ast.model.statement.SimpleAssignOperatorNode
 import inno.jago.ast.model.statement.SimpleStatementNode
 import inno.jago.cppgen.expression.binary_expression.translateToCode
 import inno.jago.cppgen.expression.translateToCode
+import inno.jago.cppgen.type.translateToCode
+import java.util.UUID
+
 fun SimpleStatementNode.translateToCode(): String = when (this) {
     is AssignmentNode -> translateToCode() + ";"
     is EmptyStatementNode -> ";"
@@ -20,19 +23,19 @@ fun SimpleStatementNode.translateToCode(): String = when (this) {
 }
 
 fun AssignmentNode.translateToCode(): String {
-    if (leftExpressions.size != rightExpressions.size) {
-        TODO("Сделать для кортежей")
-    }
-
     val translatedLeftExpressions = leftExpressions.map { it.translateToCode() }
     val translatedRightExpressions = rightExpressions.map { it.translateToCode() }
 
     var res = ""
-    translatedLeftExpressions.zip(translatedRightExpressions).forEach { (lhs, rhs) ->
-        res += when (val it = assignOperator) {
-            is SimpleAssignOperatorNode -> "$lhs = $rhs"
-            is AddOpSimpleAssignOperatorNode -> "$lhs ${it.addOperator.translateToCode()}= $rhs"
-            is MulOpSimpleAssignOperatorNode -> "$lhs ${it.mulOperator.translateToCode()}= $rhs"
+    if (leftExpressions.size != rightExpressions.size) {
+        res = "tie(${leftExpressions.joinToString()}) = ${rightExpressions.first()}"
+    } else {
+        translatedLeftExpressions.zip(translatedRightExpressions).forEach { (lhs, rhs) ->
+            res += when (val it = assignOperator) {
+                is SimpleAssignOperatorNode -> "$lhs = $rhs"
+                is AddOpSimpleAssignOperatorNode -> "$lhs ${it.addOperator.translateToCode()}= $rhs"
+                is MulOpSimpleAssignOperatorNode -> "$lhs ${it.mulOperator.translateToCode()}= $rhs"
+            }
         }
     }
 
@@ -47,6 +50,11 @@ fun IncDecStatementNode.translateToCode(): String = when (type) {
 fun ShortVarDeclNode.translateToCode(): String {
     if (identifierList.size != expression.size) {
         TODO("Сделать для кортежей")
+//        uniqueName = UUID.randomUUID().toString();
+//        res += "auto $uniqueName = ${rightExpressions.first()}"
+//        for (i in 0 until this.leftExpressions.size) {
+//            res += ""
+//        }
     }
 
     var res = ""
