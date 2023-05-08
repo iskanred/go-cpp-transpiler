bfb49afebaebca3a0de3ca1e01994322ace3ecd1
iskanred <nafikov.iskander@mail.ru>
2023-05-09 00:13:30 +0300

    test: add tests for var declaration typecheck

---
+++ src/test/resources/tests/type_checker/variable/invalid_var_declaration_4.go
@@ -0,0 +1,9 @@
+package main
+
+func someFunc() int {
+    return 1
+}
+
+func main(){
+    var a, b int = someFunc()
+}
---
+++ src/test/resources/tests/type_checker/variable/invalid_var_declaration_5.go
@@ -0,0 +1,8 @@
+package main
+
+func someFunc() {
+}
+
+func main(){
+    var a int = someFunc()
+}
