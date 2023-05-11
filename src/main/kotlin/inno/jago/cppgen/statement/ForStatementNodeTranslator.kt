5c095c48f2f695be8ef0889b3f8128eedbafe7f3
iskanred <nafikov.iskander@mail.ru>
2023-05-11 22:20:16 +0300

    refactor(translator): beatify for statement

--- src/main/kotlin/inno/jago/cppgen/statement/ForStatementNodeTranslator.kt
+++ src/main/kotlin/inno/jago/cppgen/statement/ForStatementNodeTranslator.kt
@@ -1,7 +1,6 @@
 package inno.jago.cppgen.statement
 
 import inno.jago.ast.model.statement.ConditionalForStatementNode
-import inno.jago.ast.model.statement.EmptyStatementNode
 import inno.jago.ast.model.statement.ForClauseStatementNode
 import inno.jago.ast.model.statement.ForStatementNode
 import inno.jago.cppgen.expression.translateToCode
@@ -15,10 +14,14 @@ fun ConditionalForStatementNode.translateToCode(): String {
     return "while (${condition.translateToCode()}) ${block.translateToCode()}"
 }
 
-fun ForClauseStatementNode.translateToCode(): String {
+fun ForClauseStatementNode.translateToCode(): String =
     if (initStatementNode == null && condition == null && postStatementNode == null) {
-        return "while (true) ${block.translateToCode()}"
+        "while (true) ${block.translateToCode()}"
+    } else {
+        "for (" +
+                (initStatementNode?.translateToCode() ?: "") +
+                (condition?.translateToCode() ?: "") + "; " +
+                (postStatementNode?.translateToCode()?.dropLast(1) ?: "") +
+        ") " +
+        block.translateToCode()
     }
-
-    return "for (${initStatementNode?.translateToCode() ?: ""}" +"${condition?.translateToCode() ?: ""}; ${postStatementNode?.translateToCode()?.dropLast(1) ?: ""}) ${block.translateToCode()}"
-}
